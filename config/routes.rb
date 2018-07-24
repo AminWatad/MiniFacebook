93aef5107776abbe30c144012b5c57d6ad069f7b
Amin Watad <aminwatad@Amins-MacBook-Pro.local>
2018-07-24 15:40:50 -0700

    added profile controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'profile/show'
   authenticated :user do
     root to: 'home#index', as: :authenticated_root
   end
   root to: redirect('users/sign_in')
   devise_for :users, 
-            controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+            controllers: { omniauth_callbacks: 'users/omniauth_callbacks',
+                           registrations: 'users/registrations'}
   resources :users, only: :show
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_24_014607) do
+ActiveRecord::Schema.define(version: 2018_07_24_212259) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2018_07_24_014607) do
     t.datetime "updated_at", null: false
     t.string "provider"
     t.string "uid"
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
