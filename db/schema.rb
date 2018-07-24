8b842de8147f2f4f3239b5073af420647046cc7a
Amin Watad <aminwatad@Amins-MacBook-Pro.local>
2018-07-24 12:21:40 -0700

    Added name field to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_24_175333) do
+ActiveRecord::Schema.define(version: 2018_07_24_014607) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -27,7 +28,6 @@ ActiveRecord::Schema.define(version: 2018_07_24_175333) do
     t.datetime "updated_at", null: false
     t.string "provider"
     t.string "uid"
-    t.string "name", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
